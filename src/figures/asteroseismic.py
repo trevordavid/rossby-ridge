6f3fd34ba611c154d986b39ba9d83fab1485f081
Trevor David <trevorjdavid@gmail.com>
2022-03-23 15:02:15 -0400

    referee comments

--- src/figures/asteroseismic.py
+++ src/figures/asteroseismic.py
@@ -65,27 +65,48 @@ sns.set(font_scale=1.2, context="paper", style="ticks")
 
 mpl.rcParams["legend.markerscale"] = 2
 titles = ["California–Kepler Survey","LAMOST–McQuillan"]
-ebar_kws = {"fmt":"o", "lw":1, "ms": 4, "ecolor":"orange", "color":"orange", "alpha":1, "mec":"grey", "mew":0.5}
+ebar_kws = {"fmt":"o", "lw":1, "ecolor":"orange", "color":"orange", "alpha":1, "ms":4, "mec":"grey", "mew":0.5, "zorder":999}
 
 sc_kws = {"marker":".", "color":"C0", "rasterized":True, "alpha":1}
 
-fig, ax = plt.subplots(nrows=2,ncols=1,figsize=(4.5,6.5))
+fig, (ax1,ax2) = plt.subplots(nrows=2,ncols=2,figsize=(2*4.5,6.5))
 
-ax[0].scatter(cks["p20_cks_steff"], cks["d21_prot"], **sc_kws, label=r"California–Kepler Survey", zorder=999, s=4)
-ax[1].scatter(lam['Teff_lam']+41, lam['Prot'], **sc_kws, label=r"LAMOST–McQuillan", s=0.5)
+ax1[0].scatter(cks["p20_cks_steff"], cks["d21_prot"], **sc_kws, label=r"California–Kepler Survey", zorder=999, s=4)
+ax1[1].scatter(lam['Teff_lam']+41, lam['Prot'], **sc_kws, label=r"LAMOST–McQuillan", s=0.5)
 #Note: the 41 K offset is derived in the appendix
+
+ax2[0].scatter(cks["p20_cks_steff"], cks["d21_prot"], **sc_kws, label=r"California–Kepler Survey", zorder=999, s=4)
+ax2[1].scatter(lam['Teff_lam'], lam['Prot'], **sc_kws, label=r"LAMOST–McQuillan", s=0.5)
+
+
+#Masuda et al. 2021 data
+specteff = np.array([5996.3610916725,6163.47095871239,6351.57452764171])
+specrot= np.array([15.1398954901294,10.9922199999722,5.40119754702563])
+spectefferr = np.array([[81.4555633310,58.78236529041,103.289013296011],[103.289013296011,79.77606717985,282.15535339398]])
+specroterr = np.array([[5.1308763552480,4.27108719012285,2.39406040514287],[8.1215420545671,7.1261658513948,4.30003441066639]])
 
 for i in range(2):
     arg = hall["Type"] == "MS"
-    ax[i].errorbar(hall["Teff"][arg], hall["P"][arg], 
+    ax1[i].errorbar(hall["Teff"][arg], hall["P"][arg], 
                    xerr=hall["e_Teff"][arg], 
                    yerr=[hall["e_P"][arg], hall["E_P"][arg]], 
                    label="Hall et al. 2021\nasteroseismic sample",
                    **ebar_kws)
-    ax[i].set_xlim(6500,5000)
-    ax[i].set_xlabel("Effective temperature [K]")
-    ax[i].set_ylabel("Rotation period [d]")
-    ax[i].legend(prop={"size":9})
+    ax2[i].errorbar(specteff, specrot, 
+                    xerr=spectefferr,
+                    yerr=specroterr,
+                    label="Masuda et al. 2021",
+                    **ebar_kws)
+    ax1[i].set_xlim(6500,5000)
+    ax1[i].set_xlabel("Effective temperature [K]")
+    ax1[i].set_ylabel("Rotation period [d]")
+    ax1[i].legend(loc="upper left", prop={"size":9})
+
+    ax2[i].set_xlim(6500,5000)
+    ax2[i].set_ylim(0,50)
+    ax2[i].set_xlabel("Effective temperature [K]")
+    ax2[i].set_ylabel("Rotation period [d]")
+    ax2[i].legend(loc="upper left", prop={"size":9})    
     
 sns.despine()    
 plt.tight_layout()
